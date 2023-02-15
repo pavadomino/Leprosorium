cf14f664733a3ffe425db0893888dac04791099d
pavadomino <pavadomino@gmail.com>
2023-02-15 23:52:57 +0100

    Add text area to /details/...

--- app.rb
+++ app.rb
@@ -51,6 +51,7 @@ end
 
 get '/details/:post_id' do
   post_id = params[:post_id] # parameter from url
-
-  erb "Displaying information for post with id #{post_id}"
+  results = @db.execute 'select * from Posts where id = ?', [post_id]
+  @row = results[0]
+  erb :details
 end
